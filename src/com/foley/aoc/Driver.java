8f653326086cb4c5b52ea9c87caf1b8de9977f4a
heckler82 <heckler82@gmail.com>
2020-11-28 16:59:41 -0500

    ==== General ====
    ==== Additions ====
    
    ==== Driver.java ====
    - User can now configure to run 1 or all days via CLI or JOptionPane

--- src/com/foley/aoc/Driver.java
+++ src/com/foley/aoc/Driver.java
@@ -20,6 +20,7 @@ public class Driver {
     public static void main(String[] args) {
         // Parse the current year
         int year = -1;
+        int day = -1;
 
         // If year was provided on the command line, attempt to parse it
         if(args.length > 0) {
@@ -29,10 +30,26 @@ public class Driver {
                 System.err.printf("Cannot parse \"%s\" into a number\n", args[0]);
                 System.exit(1);
             }
+
+            // An additional input is the requested day
+            if(args.length > 1) {
+                // Determine if user wants all days, or only 1 day to run
+                if("all".equalsIgnoreCase(args[1])) {
+                    day = 0;
+                } else {
+                    // Parse number from argument
+                    try {
+                        day = Integer.parseInt(args[1]);
+                    } catch(NumberFormatException e) {
+                        System.err.printf("Cannot parse \"%s\" into a number\n", args[1]);
+                        System.exit(1);
+                    }
+                }
+            }
         }
 
         // Ensure a valid year was provided if command line argument was not provided
-        while(year == -1) {
+        while(year < 0) {
             String in = JOptionPane.showInputDialog(null, "What is the current year (Enter exit or press cancel to exit)?", "Enter Current Year", JOptionPane.PLAIN_MESSAGE);
             // Determine exit cases
             if("exit".equalsIgnoreCase(in) || in == null) {
@@ -46,10 +63,40 @@ public class Driver {
             }
         }
 
-        // Get and run all days for the year
-        for(int i = 1; i < 26; i++) {
-            String inputPath = "./res/" + year + "/day" + i + ".txt";
-            String className = "com.foley.aoc.year" + year + ".Day" + i;
+        // Ensure a valid day was provided if command line argument was not provided
+        while(day < 0 || day >25) {
+            String in = JOptionPane.showInputDialog(null, "What day would you like to run (Enter 1-25 or \"all\")?", "Enter Desired Day", JOptionPane.PLAIN_MESSAGE);
+            // Determine exit cases
+            if(in == null) {
+                continue;
+            }
+
+            // Short out for all days requested
+            if("all".equalsIgnoreCase(in)) {
+                day = 0;
+                break;
+            }
+
+            // Attempt to parse the input to a numerical day value
+            try {
+                day = Integer.parseInt(in);
+            } catch(NumberFormatException e) {
+                System.err.printf("Cannot parse \"%s\" into a number\n", in);
+            }
+        }
+
+        // Run all days or only the requested day
+        if(day == 0) {
+            // Get and run all days for the year
+            for(int i = 1; i < 26; i++) {
+                String inputPath = "./res/" + year + "/day" + i + ".txt";
+                String className = "com.foley.aoc.year" + year + ".Day" + i;
+                Daily d = Functions.getDaily(inputPath, className);
+                d.doTasks();
+            }
+        } else {
+            String inputPath = "./res/" + year + "/day" + day + ".txt";
+            String className = "com.foley.aoc.year" + year + ".Day" + day;
             Daily d = Functions.getDaily(inputPath, className);
             d.doTasks();
         }
