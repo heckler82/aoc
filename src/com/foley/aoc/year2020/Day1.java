489e3c5e56a5e168b6af5fe57d65a4896f169ef4
heckler82 <heckler82@gmail.com>
2020-12-01 08:09:09 -0500

    Update Day1.java
    Added the solution for part 2

--- src/com/foley/aoc/year2020/Day1.java
+++ src/com/foley/aoc/year2020/Day1.java
@@ -2,8 +2,11 @@ package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
 import com.foley.aoc.util.Functions;
+import com.foley.aoc.util.Tuple;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Solutions for day 1
@@ -12,6 +15,8 @@ import java.util.Arrays;
  * @version 24 Nov 2020
  */
 public class Day1 extends Daily {
+    private int[] ints;
+    
     /**
      * Creates a new daily
      *
@@ -19,7 +24,8 @@ public class Day1 extends Daily {
      */
     public Day1(String fileName) {
         super(fileName);
-        //day = 1;
+        ints = convertToIntArray(input);
+        Arrays.sort(ints);
     }
 
     @Override
@@ -27,16 +33,13 @@ public class Day1 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
-        int[] ints = Functions.convertToIntArray(input);
-        Arrays.sort(ints);
-        
         // Search for value in array that will sum to 2020 with the current value
         for(int i : ints) {
             int index = Functions.binarySearch(ints, 2020 - i);
             // If index is non-negative, the answer has been found
-            if(index >= 0) {
+            if(index >= 0 && index < ints.length) {
                 System.out.printf("2020 found @ <x = %d, y = %d>\n", i, ints[index]);
-                System.out.printf("The product of the two is %d\n", i, ints[index]);
+                System.out.printf("The product of the two is %d\n", i * ints[index]);
                 return;
             }
         }
@@ -47,6 +50,24 @@ public class Day1 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
-
+        List<Tuple<Integer, Integer>> list = new ArrayList<>();
+        
+        // Naively get all possible pair combinations from the input
+        for(int i = 0; i < ints.length - 1; i++) {
+            for(int j = i + 1; j < ints.length; j++) {
+                list.add(new Tuple<>(ints[i], ints[j]));
+            }
+        }
+        
+        // Derivative of part 1
+        for(Tuple<> t : list) {
+            int index = Functions.binarySearch(ints, 2020 - (t.getFirst() + t.getSecond()));
+            // If index is non-negative, the answer has been found
+            if(index >= 0 && index < ints.length && ints[index] != t.getFirst() && ints[index] != t.getSecond()) {
+                System.out.printf("2020 found @ <x = %d, y = %d, z = %d>\n", t.getFirst(), t.getSecond(), ints[index]);
+                System.out.printf("The product of the three is %d\n", t.getFirst() * t.getSecond() * ints[index]);
+                return;
+            }
+        }
     }
 }
