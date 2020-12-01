8340cba446474b5d87715186bf0d99d5d7260456
heckler82 <heckler82@gmail.com>
2020-12-01 08:24:15 -0500

    Update Day1.java
    Removed the index < ints.length check. Functions.binarySearch(int[], int) will not return an index that satisfies that condition

--- src/com/foley/aoc/year2020/Day1.java
+++ src/com/foley/aoc/year2020/Day1.java
@@ -37,7 +37,7 @@ public class Day1 extends Daily {
         for(int i : ints) {
             int index = Functions.binarySearch(ints, 2020 - i);
             // If index is non-negative, the answer has been found
-            if(index >= 0 && index < ints.length) {
+            if(index >= 0 && index) {
                 System.out.printf("2020 found @ <x = %d, y = %d>\n", i, ints[index]);
                 System.out.printf("The product of the two is %d\n", i * ints[index]);
                 return;
@@ -63,7 +63,7 @@ public class Day1 extends Daily {
         for(Tuple<> t : list) {
             int index = Functions.binarySearch(ints, 2020 - (t.getFirst() + t.getSecond()));
             // If index is non-negative, the answer has been found
-            if(index >= 0 && index < ints.length && ints[index] != t.getFirst() && ints[index] != t.getSecond()) {
+            if(index >= 0 && ints[index] != t.getFirst() && ints[index] != t.getSecond()) {
                 System.out.printf("2020 found @ <x = %d, y = %d, z = %d>\n", t.getFirst(), t.getSecond(), ints[index]);
                 System.out.printf("The product of the three is %d\n", t.getFirst() * t.getSecond() * ints[index]);
                 return;
