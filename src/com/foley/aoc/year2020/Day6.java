e0b46c7fa89a6ec7819b3f146aa888ebb0395eac
heckler82 <heckler82@gmail.com>
2020-12-06 01:18:02 -0500

    Added day  solutions

--- src/com/foley/aoc/year2020/Day6.java
+++ src/com/foley/aoc/year2020/Day6.java
@@ -2,6 +2,9 @@ package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
 
+import java.util.Arrays;
+import java.util.BitSet;
+
 /**
  * Solutions for day 6
  *
@@ -23,7 +26,19 @@ public class Day6 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        int sum = 0;
+        StringBuilder grp = new StringBuilder();
 
+        for(int i = 0; i < input.length; i++) {
+            grp.append(input[i]);
+            if("".equals(input[i]) || i == input.length - 1) {
+                sum += grp.chars().distinct().count();
+                grp.setLength(0);
+                continue;
+            }
+        }
+
+        System.out.printf("The sum of the question counts is %d\n", sum);
     }
 
     @Override
@@ -31,6 +46,26 @@ public class Day6 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        int sum = 0;
+        int grp = Integer.MAX_VALUE;
 
+        for(int i = 0; i < input.length; i++) {
+            String s = input[i];
+            int buckets = 0;
+
+            // Activate all bits from the string and keep only repeats, exclude empty strings
+            for(char c : s.toCharArray()) {
+                buckets |= 1 << (c - 'a');
+            }
+            grp &= (s.length() == 0) ? grp : buckets;
+
+            if("".equals(s) || i == input.length - 1) {
+                sum += Integer.bitCount(grp);
+                grp = Integer.MAX_VALUE;
+                continue;
+            }
+        }
+
+        System.out.printf("The sum of the questions everyone answered yes to is %d\n", sum);
     }
 }
