9ca09409060109d56c0fe03bf0b4dcd5a3facd14
heckler82 <heckler82@gmail.com>
2020-12-07 08:44:08 -0500

    Create Regex.java
    Added  Regex.java

---
+++ src/com/foley/aoc/util/functions/Regex.java
@@ -0,0 +1,10 @@
+package com.foley.aoc.util.functions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+* Simple functions for working with regular expressions
+*/
+public class Regex {
+}
