7cb2aeaf668b48ff959ec0b9035400c374a3df19
heckler82 <heckler82@gmail.com>
2020-12-12 02:45:11 -0500

    Added day 12 solutions

--- src/com/foley/aoc/year2020/Day12.java
+++ src/com/foley/aoc/year2020/Day12.java
@@ -2,6 +2,8 @@ package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
 
+import java.awt.Point;
+
 /**
  * Solutions for day 12
  *
@@ -9,6 +11,14 @@ import com.foley.aoc.util.Daily;
  * @version 24 Nov 2020
  */
 public class Day12 extends Daily {
+    enum Direction {
+        WEST,
+        NORTH,
+        EAST,
+        SOUTH;
+    }
+    private Point p2;
+
     /**
      * Creates a new daily
      *
@@ -23,7 +33,71 @@ public class Day12 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        Point p = new Point(0, 0);
+        p2 = new Point(0, 0);
+        Point way = new Point(10, 1);
+        Direction dir = Direction.EAST;
+        Direction[] dirs = Direction.values();
 
+        for(String s : input) {
+            char action = s.charAt(0);
+            int dist = Integer.parseInt(s.substring(1));
+            Direction perm = Direction.EAST;
+            int diff;
+            switch(action) {
+                case 'F':
+                    p2.x += way.x * dist;
+                    p2.y += way.y * dist;
+                    if(dir.ordinal() % 2 == 0) {
+                        p.x += dir.ordinal() == 2 ? dist : -dist;
+                        //p2.x += dir.ordinal() == 2 ? (way.x * dist) : -(way.x * dist);
+                    } else {
+                        p.y += dir.ordinal() == 1 ? dist : -dist;
+                        //p2.y += dir.ordinal() == 1 ? (way.y * dist) : -(way.y * dist);
+                    }
+                    break;
+                case 'N':
+                    p.y += dist;
+                    way.y += dist;
+                    break;
+                case 'S':
+                    p.y -= dist;
+                    way.y -= dist;
+                    break;
+                case 'E':
+                    p.x += dist;
+                    way.x += dist;
+                    break;
+                case 'W':
+                    p.x -= dist;
+                    way.x -= dist;
+                    break;
+                case 'L':
+                    diff = dir.ordinal() - (dist / 90);
+                    if(diff < 0) {
+                        diff += dirs.length;
+                    }
+                    dir = dirs[diff];
+                    for(int i = 0; i < (dist / 90); i++) {
+                        int temp = way.x;
+                        way.x = -way.y;
+                        way.y = temp;
+                    }
+                    break;
+                case 'R':
+                    diff = dir.ordinal() + (dist / 90);
+                    if(diff >= dirs.length) {
+                        diff -= dirs.length;
+                    }
+                    dir = dirs[diff];
+                    for(int i = 0; i < (dist / 90); i++) {
+                        int temp = way.y;
+                        way.y = -way.x;
+                        way.x = temp;
+                    }
+            }
+        }
+        System.out.printf("The distance is %d\n", Math.abs(p.x) + Math.abs(p.y));
     }
 
     @Override
@@ -31,6 +105,6 @@ public class Day12 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
-
+        System.out.printf("The distance is %d\n", Math.abs(p2.x) + Math.abs(p2.y));
     }
 }
