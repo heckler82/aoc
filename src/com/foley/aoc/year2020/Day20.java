cf3372c33bb743be33f24b7003f6861b64bb81b7
Evan <heckler82@gmail.com>
2020-12-22 06:32:45 -0500

    ugh

--- src/com/foley/aoc/year2020/Day20.java
+++ src/com/foley/aoc/year2020/Day20.java
@@ -1,6 +1,11 @@
 package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
+import com.foley.aoc.util.functions.Regex;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Solutions for day 20
@@ -9,6 +14,8 @@ import com.foley.aoc.util.Daily;
  * @version 24 Nov 2020
  */
 public class Day20 extends Daily {
+    private Map<Integer, char[][]> map;
+
     /**
      * Creates a new daily
      *
@@ -16,6 +23,7 @@ public class Day20 extends Daily {
      */
     public Day20(String fileName) {
         super(fileName);
+        map = parseInput(input);
     }
 
     @Override
@@ -23,7 +31,6 @@ public class Day20 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
-
     }
 
     @Override
@@ -31,6 +38,28 @@ public class Day20 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+    }
 
+    private Map<Integer, char[][]> parseInput(String[] input) {
+        Map<Integer, char[][]> map = new HashMap<>();
+        int currentId = 0;
+        int currentRow = 0;
+        char[][] chars = new char[10][];
+        for(String s : input) {
+            if(s.contains("Tile ")) {
+                currentId = Integer.parseInt(Regex.getSingleMatch("(\\d+)", s));
+                continue;
+            }
+            if("".equals(s)) {
+                map.put(currentId, chars);
+                chars = new char[10][];
+                currentRow = 0;
+                continue;
+            }
+            chars[currentRow] = s.toCharArray();
+            currentRow++;
+        }
+        map.put(currentId, chars);
+        return map;
     }
 }
