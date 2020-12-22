3774b3db0937fcbe2f8954ce43a0e9792d309c58
Evan <heckler82@gmail.com>
2020-12-22 08:07:08 -0500

    Added day 22 part 1 solution

--- src/com/foley/aoc/year2020/Day22.java
+++ src/com/foley/aoc/year2020/Day22.java
@@ -2,6 +2,8 @@ package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
 
+import java.util.*;
+
 /**
  * Solutions for day 22
  *
@@ -9,6 +11,10 @@ import com.foley.aoc.util.Daily;
  * @version 24 Nov 2020
  */
 public class Day22 extends Daily {
+    private int deckSize;
+    private List<Integer> p1;
+    private List<Integer> p2;
+
     /**
      * Creates a new daily
      *
@@ -16,6 +22,20 @@ public class Day22 extends Daily {
      */
     public Day22(String fileName) {
         super(fileName);
+        p1 = new ArrayList<>();
+        p2 = new ArrayList<>();
+
+        List<Integer>  curr = p1;
+        for(int i = 1; i < input.length; i++) {
+            String s = input[i];
+            if("".equals(s)) {
+                curr = p2;
+                i += 1;
+                continue;
+            }
+            curr.add(Integer.parseInt(s));
+        }
+        deckSize = p1.size() + p2.size();
     }
 
     @Override
@@ -23,7 +43,13 @@ public class Day22 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        // Continue until a winner is found
+        while(p1.size() != deckSize && p2.size() != deckSize) {
+            takeTurn();
+        }
 
+        List<Integer> winner = p1.size() == deckSize ? p1 : p2;
+        System.out.printf("The winning score is %d\n", calcWinningScore(winner));
     }
 
     @Override
@@ -31,6 +57,25 @@ public class Day22 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+    }
 
+    private void takeTurn() {
+        int c1 = p1.remove(0);
+        int c2 = p2.remove(0);
+        if(c1 > c2) {
+            p1.add(c1);
+            p1.add(c2);
+        } else {
+            p2.add(c2);
+            p2.add(c1);
+        }
+    }
+
+    private long calcWinningScore(List<Integer> winner) {
+        long sum = 0;
+        for(int i = deckSize; i > 0; i--) {
+            sum += i * winner.remove(0);
+        }
+        return sum;
     }
 }
