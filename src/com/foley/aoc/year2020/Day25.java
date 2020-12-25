dc8e3d04706704830bbb70f87b894177ca0f67fe
Evan <heckler82@gmail.com>
2020-12-25 07:45:06 -0500

    Added day 25 solution

--- src/com/foley/aoc/year2020/Day25.java
+++ src/com/foley/aoc/year2020/Day25.java
@@ -9,6 +9,13 @@ import com.foley.aoc.util.Daily;
  * @version 24 Nov 2020
  */
 public class Day25 extends Daily {
+    private int doorLoop;
+    private int cardLoop;
+    private int doorKey;
+    private int cardKey;
+    private final int SUBJ = 7;
+    private final int HASH = 20201227;
+
     /**
      * Creates a new daily
      *
@@ -16,6 +23,12 @@ public class Day25 extends Daily {
      */
     public Day25(String fileName) {
         super(fileName);
+        cardKey = Integer.parseInt(input[0]);
+        doorKey = Integer.parseInt(input[1]);
+        //cardKey = 5764801;
+        //doorKey = 17807724;
+        cardLoop = findLoopSize(cardKey);
+        doorLoop = findLoopSize(doorKey);
     }
 
     @Override
@@ -23,7 +36,11 @@ public class Day25 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
-
+        long res = transform(cardKey, doorLoop);
+        if(res != transform(doorKey, cardLoop)) {
+            System.out.printf("The encryption key is not correct\n");
+        }
+        System.out.printf("Encryption key: %d\n", res);
     }
 
     @Override
@@ -31,6 +48,27 @@ public class Day25 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        System.out.println("01001101 01100101 01110010 01110010 01111001 00100000 01000011 01101000 01110010 01101001 01110011 01110100 01101101 01100001 01110011");
+    }
 
+    private int findLoopSize(int in) {
+        int count = 0;
+        int val = 1;
+        // Transform until final value reached
+        while(val != in) {
+            val *= SUBJ;
+            val %= HASH;
+            count++;
+        }
+        return count;
+    }
+
+    private long transform(int key, int loop) {
+        long val = 1;
+        for(int i = 0; i < loop; i++) {
+            val *= key;
+            val %= HASH;
+        }
+        return val;
     }
 }
