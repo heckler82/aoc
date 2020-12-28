e4853ca51dd3b577b7ac0395d54ab5da1e3743a9
heckler82 <heckler82@gmail.com>
2020-12-28 15:03:22 -0500

    Removed day 20 because I have no idea how to tackle it

--- src/com/foley/aoc/year2020/Day20.java
+++ src/com/foley/aoc/year2020/Day20.java
@@ -1,11 +1,6 @@
 package com.foley.aoc.year2020;
 
 import com.foley.aoc.util.Daily;
-import com.foley.aoc.util.functions.Regex;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Solutions for day 20
@@ -14,7 +9,6 @@ import java.util.Map;
  * @version 24 Nov 2020
  */
 public class Day20 extends Daily {
-    private Map<Integer, char[][]> map;
 
     /**
      * Creates a new daily
@@ -23,7 +17,6 @@ public class Day20 extends Daily {
      */
     public Day20(String fileName) {
         super(fileName);
-        map = parseInput(input);
     }
 
     @Override
@@ -38,28 +31,5 @@ public class Day20 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
-    }
-
-    private Map<Integer, char[][]> parseInput(String[] input) {
-        Map<Integer, char[][]> map = new HashMap<>();
-        int currentId = 0;
-        int currentRow = 0;
-        char[][] chars = new char[10][];
-        for(String s : input) {
-            if(s.contains("Tile ")) {
-                currentId = Integer.parseInt(Regex.getSingleMatch("(\\d+)", s));
-                continue;
-            }
-            if("".equals(s)) {
-                map.put(currentId, chars);
-                chars = new char[10][];
-                currentRow = 0;
-                continue;
-            }
-            chars[currentRow] = s.toCharArray();
-            currentRow++;
-        }
-        map.put(currentId, chars);
-        return map;
     }
 }
