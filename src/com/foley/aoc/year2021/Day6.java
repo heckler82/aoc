a6fcf5699836f760285546c746d99b6222058fc6
heckler82 <heckler82@gmail.com>
2021-12-05 20:37:22 -0900

    Added day 6 solution

--- src/com/foley/aoc/year2021/Day6.java
+++ src/com/foley/aoc/year2021/Day6.java
@@ -1,6 +1,9 @@
 package com.foley.aoc.year2021;
 
 import com.foley.aoc.util.Daily;
+import com.foley.aoc.util.functions.Regex;
+
+import java.util.regex.Matcher;
 
 /**
  * Solutions for day 1
@@ -9,6 +12,8 @@ import com.foley.aoc.util.Daily;
  * @version 06 Dec 2021
  */
 public class Day6 extends Daily {
+    private long[] fishTimers = new long[9];
+
     /**
      * Creates a new daily
      *
@@ -16,6 +21,10 @@ public class Day6 extends Daily {
      */
     public Day6(String fileName) {
         super(fileName);
+        Matcher m = Regex.getMatcher("\\d+", input[0]);
+        while(m.find()) {
+            fishTimers[Integer.parseInt(m.group())]++;
+        }
     }
 
     @Override
@@ -23,6 +32,7 @@ public class Day6 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        System.out.printf("There are %d fish after 80 days\n", simulate(80));
     }
 
     @Override
@@ -30,5 +40,33 @@ public class Day6 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        System.out.printf("There are %d fish after 256 days\n", simulate(256 - 80));
+    }
+
+    /**
+     * Simulates the fish world
+     *
+     * @param numDays the number of days to simluate
+     * @return the number of fish at the end of the simmulation
+     */
+    private long simulate(int numDays) {
+        for(int i = 0; i < numDays; i++) {
+            long[] nextGen = new long[9];
+            for(int t = 8; t > -1; t--) {
+                if(t == 0) {
+                    nextGen[8] = fishTimers[0];
+                    nextGen[6] += fishTimers[0];
+                } else {
+                    nextGen[t - 1] = fishTimers[t];
+                }
+            }
+            fishTimers = nextGen;
+        }
+        // Get grand total
+        long sum = 0L;
+        for(long l : fishTimers) {
+            sum += l;
+        }
+        return sum;
     }
 }
