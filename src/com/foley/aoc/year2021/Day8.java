23ba32616c192280ebc8a914df6aae8304143ad6
heckler82 <heckler82@gmail.com>
2021-12-08 19:18:42 -0900

    Put the decoding logic into its own class

--- src/com/foley/aoc/year2021/Day8.java
+++ src/com/foley/aoc/year2021/Day8.java
@@ -5,7 +5,9 @@ import com.foley.aoc.util.Daily;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.*;
 
@@ -37,65 +39,16 @@ public class Day8 extends Daily {
         for(String s : input) {
             var cache = new HashMap<Integer, Set<Character>>();
             var inputs = Arrays.asList(s.split(" \\| ")[0].split("\\s+"));
-            inputs.sort((String s1, String s2) -> (s1.length() == 5 && s2.length() == 6) ? s2.length() - s1.length() : (s1.length() == 6 && s2.length() == 5 ? s2.length() - s1.length() : s1.length() - s2.length()));
-            for(String in : inputs) {
-                char[] arr = in.toCharArray();
-                var set = createCharSet(arr);
-                switch(in.length()) {
-                    case 2: // 1
-                        cache.put(1, set);
-                        break;
-                    case 3: // 7
-                        cache.put(7, set);
-                        break;
-                    case 4: // 4
-                        cache.put(4, set);
-                        break;
-                    case 5: // 2, 3, or 5
-                        if(set.containsAll(cache.get(1)) && set.containsAll(cache.get(7))) {
-                            cache.put(3, set);
-                        } else {
-                            // 2 or 5
-                            if(cache.get(6).containsAll(set)) {
-                                cache.put(5, set);
-                            } else {
-                                cache.put(2, set);
-                            }
-                        }
-                        break;
-                    case 6: // 0, 6, or 9
-                        if(set.containsAll(cache.get(1)) && set.containsAll(cache.get(7))) {
-                            if(set.containsAll(cache.get(4))) {
-                                cache.put(9, set);
-                            } else {
-                                cache.put(0, set);
-                            }
-                        } else {
-                            cache.put(6, set);
-                        }
-                        break;
-                    case 7: // 8
-                        cache.put(8, set);
-                }
-            }
-            String[] output = s.split(" \\| ")[1].split("\\s+");
-            StringBuilder decode = new StringBuilder();
+            Decoder decoder = new Decoder();
+            decoder.seed(inputs);
+            var output = Arrays.asList(s.split(" \\| ")[1].split("\\s+"));
+            decoded.add(decoder.decode(output));
+
             for(String o : output) {
                 if(o.length() == 2 || o.length() == 3 || o.length() == 4 || o.length() == 7) {
                     count++;
                 }
-                for(var key : cache.keySet()) {
-                    if(cache.get(key).size() == o.length()) {
-                        var oSet = createCharSet(o.toCharArray());
-                        oSet.retainAll(cache.get(key));
-                        if(oSet.size() == o.length()) {
-                            decode.append(key);
-                            break;
-                        }
-                    }
-                }
             }
-            decoded.add(Integer.parseInt(decode.toString()));
         }
         System.out.printf("The digits 1, 4, 7, and 8 appear %d times\n", count);
     }
@@ -112,9 +65,100 @@ public class Day8 extends Daily {
         System.out.printf("The final sum is %d\n", sum);
     }
 
-    private Set<Character> createCharSet(char[] arr) {
-        return IntStream.range(0, arr.length)
-                .mapToObj(i -> arr[i])
-                .collect(Collectors.toSet());
+    /**
+     * Creates a new decoder
+     */
+    private class Decoder {
+        private Map<Integer, Set<Character>> digiPatterns;
+
+        public Decoder() {
+            digiPatterns = new HashMap<>();
+        }
+
+        /**
+         * Seeds the decoder with values for unscrambling digits
+         *
+         * @param seeds the seed values
+         */
+        public void seed(List<String> seeds) {
+            seeds.sort((String s1, String s2) -> s1.length() - s2.length());
+            for(String s : seeds) {
+                var set = createCharSet(s.toCharArray());
+                switch(s.length()) {
+                    case 2: // 1
+                        digiPatterns.put(1, set);
+                        break;
+                    case 3: // 7
+                        digiPatterns.put(7, set);
+                        break;
+                    case 4: // 4
+                        digiPatterns.put(4, set);
+                        break;
+                    case 5: // 2, 3, or 5
+                        if(set.containsAll(digiPatterns.get(1))) {
+                            digiPatterns.put(3, set);
+                            break;
+                        }
+                        var key = new HashSet<Character>(digiPatterns.get(4));
+                        key.removeAll(digiPatterns.get(1));
+                        if(set.containsAll(key)) {
+                            digiPatterns.put(5, set);
+                            break;
+                        }
+                        digiPatterns.put(2, set);
+                        break;
+                    case 6: // 0, 6, or 9
+                        if(!set.containsAll(digiPatterns.get(1))) {
+                            digiPatterns.put(6, set);
+                            break;
+                        }
+                        key = new HashSet<Character>(digiPatterns.get(4));
+                        key.removeAll(digiPatterns.get(1));
+                        if(set.containsAll(key)) {
+                            digiPatterns.put(9, set);
+                            break;
+                        }
+                        digiPatterns.put(0, set);
+                        break;
+                    case 7: // 8
+                        digiPatterns.put(8, set);
+                }
+            }
+        }
+
+        /**
+         * Decodes a list of input strings into a single integer value
+         *
+         * @param decode the input strings
+         * @return the integer representation of the input strings
+         */
+        public int decode(List<String> decode) {
+            StringBuilder sb = new StringBuilder();
+            for(String s : decode) {
+                for(var key : digiPatterns.keySet()) {
+                    if(digiPatterns.get(key).size() == s.length()) {
+                        var oSet = createCharSet(s.toCharArray());
+                        oSet.retainAll(digiPatterns.get(key));
+                        if(oSet.size() == s.length()) {
+                            sb.append(key);
+                            break;
+                        }
+                    }
+                }
+            }
+            return Integer.parseInt(sb.toString());
+        }
+
+        /**
+         * Creates a set made up of the unique characters of the input array
+         *
+         * @param arr the input array
+         * @return a set of unique characters from the input array
+         */
+        private Set<Character> createCharSet(char[] arr) {
+            return IntStream.range(0, arr.length)
+                    .mapToObj(i -> arr[i])
+                    .collect(Collectors.toSet());
+        }
     }
 }
