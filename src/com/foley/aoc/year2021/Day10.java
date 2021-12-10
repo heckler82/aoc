c509a3309d2f8e3324c580b7a5e8fa0975aa1db8
heckler82 <heckler82@gmail.com>
2021-12-09 20:35:59 -0900

    Added day 10 solution

--- src/com/foley/aoc/year2021/Day10.java
+++ src/com/foley/aoc/year2021/Day10.java
@@ -1,6 +1,9 @@
 package com.foley.aoc.year2021;
 
 import com.foley.aoc.util.Daily;
+import com.foley.aoc.util.Tuple;
+
+import java.util.*;
 
 /**
  * Solutions for day 1
@@ -9,6 +12,10 @@ import com.foley.aoc.util.Daily;
  * @version 10 Dec 2021
  */
 public class Day10 extends Daily {
+    private Map<Character, Tuple<Character, Integer>> illegal;
+    private Map<Character, Integer> close;
+    List<String> consider;
+
     /**
      * Creates a new daily
      *
@@ -16,6 +23,19 @@ public class Day10 extends Daily {
      */
     public Day10(String fileName) {
         super(fileName);
+        illegal = new HashMap<>();
+        illegal.put(')', Tuple.pair('(', 3));
+        illegal.put(']', Tuple.pair('[', 57));
+        illegal.put('}', Tuple.pair('{', 1197));
+        illegal.put('>', Tuple.pair('<', 25137));
+
+        close = new HashMap<>();
+        close.put('(', 1);
+        close.put('[', 2);
+        close.put('{', 3);
+        close.put('<', 4);
+
+        consider = new ArrayList<>();
     }
 
     @Override
@@ -23,6 +43,24 @@ public class Day10 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        int errorSum = 0;
+        for(String s : input) {
+            consider.add(s);
+            Stack<Character> open = new Stack<>();
+            for(char c : s.toCharArray()) {
+                if(illegal.containsKey(c)) {
+                    char t = open.pop();
+                    if(illegal.get(c).getFirst() != t) {
+                        errorSum += illegal.get(c).getSecond();
+                        consider.remove(s);
+                        break;
+                    }
+                } else {
+                    open.push(c);
+                }
+            }
+        }
+        System.out.printf("The total syntax error score is %d\n", errorSum);
     }
 
     @Override
@@ -30,5 +68,25 @@ public class Day10 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        List<Long> scores = new ArrayList<>();
+        for(String s : consider) {
+            Stack<Character> open = new Stack<>();
+            for(char c : s.toCharArray()) {
+                if(illegal.containsKey(c)) {
+                    open.pop();
+                    continue;
+                }
+                open.push(c);
+            }
+
+            long total = 0L;
+            while(!open.isEmpty()) {
+                char c = open.pop();
+                total = 5 * total + close.get(c);
+            }
+            scores.add(total);
+        }
+        scores.sort(Comparator.naturalOrder());
+        System.out.printf("The middle score is %d\n", scores.get(scores.size() / 2));
     }
 }
