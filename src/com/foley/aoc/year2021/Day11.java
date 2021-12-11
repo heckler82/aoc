3829f8fdf43e81c919218cb1c750cbf0d2ce3ae4
heckler82 <heckler82@gmail.com>
2021-12-10 21:47:32 -0900

    Generalized solution to prevent a part2 answer that should be less than 100

--- src/com/foley/aoc/year2021/Day11.java
+++ src/com/foley/aoc/year2021/Day11.java
@@ -14,6 +14,7 @@ import java.util.Set;
  */
 public class Day11 extends Daily {
     private int[][] map;
+    private int[][] map2;
 
     /**
      * Creates a new daily
@@ -23,9 +24,11 @@ public class Day11 extends Daily {
     public Day11(String fileName) {
         super(fileName);
         map = new int[10][10];
+        map2 = new int[10][10];
         for(int y = 0; y < 10; y++) {
             for(int x = 0; x < 10; x++) {
                 map[y][x] = input[y].charAt(x) - '0';
+                map2[y][x] = input[y].charAt(x) - '0';
             }
         }
     }
@@ -37,7 +40,7 @@ public class Day11 extends Daily {
     public void task1() {
         int count = 0;
         for(int t = 0; t < 100; t++) {
-            count += sim();
+            count += sim(map);
         }
         System.out.printf("The number of flashes in 100 steps is %d\n", count);
     }
@@ -50,13 +53,13 @@ public class Day11 extends Daily {
         int step = 0;
         int result = 0;
         while(result != 100) {
-            result = sim();
+            result = sim(map2);
             step++;
         }
-        System.out.printf("The first time all the octopi flash simultaneously is step %d\n", step + 100);
+        System.out.printf("The first time all the octopi flash simultaneously is step %d\n", step);
     }
 
-    private int sim() {
+    private int sim(int[][] map) {
         int count = 0;
         // Increment
         for(int y = 0; y < 10; y++) {
@@ -76,14 +79,14 @@ public class Day11 extends Daily {
                         flashed.add(p);
                         count++;
                         allFlashed = false;
-                        increment(x - 1, y - 1);
-                        increment(x, y - 1);
-                        increment(x + 1, y - 1);
-                        increment(x - 1, y);
-                        increment(x + 1, y);
-                        increment(x - 1, y + 1);
-                        increment(x, y + 1);
-                        increment(x + 1, y + 1);
+                        increment(x - 1, y - 1, map);
+                        increment(x, y - 1, map);
+                        increment(x + 1, y - 1, map);
+                        increment(x - 1, y, map);
+                        increment(x + 1, y, map);
+                        increment(x - 1, y + 1, map);
+                        increment(x, y + 1, map);
+                        increment(x + 1, y + 1, map);
                     }
                 }
             }
@@ -95,19 +98,9 @@ public class Day11 extends Daily {
         return count;
     }
 
-    private void increment(int x, int y) {
+    private void increment(int x, int y, int[][] map) {
         if((y >= 0 && y < 10) && (x >= 0 && x < 10)) {
             map[y][x]++;
-        }
-    }
-
-    private void printMap() {
-        System.out.println();
-        for(int[] arr : map) {
-            for(int i : arr) {
-                System.out.print(i);
-            }
-            System.out.println();
         }
     }
 }
