404d7fa7971b84a9c8817d78dc5cfaff8b328ce2
heckler82 <heckler82@gmail.com>
2021-12-11 21:47:11 -0900

    Added day 12 solution

--- src/com/foley/aoc/year2021/Day12.java
+++ src/com/foley/aoc/year2021/Day12.java
@@ -1,14 +1,20 @@
 package com.foley.aoc.year2021;
 
 import com.foley.aoc.util.Daily;
+import com.foley.aoc.util.graph.Graph;
+import com.foley.aoc.util.graph.HashGraph;
+
+import java.util.*;
 
 /**
- * Solutions for day 1
+ * Solutions for day 12
  *
  * @author Evan Foley
  * @version 12 Dec 2021
  */
 public class Day12 extends Daily {
+    private Graph<String> g;
+
     /**
      * Creates a new daily
      *
@@ -16,6 +22,13 @@ public class Day12 extends Daily {
      */
     public Day12(String fileName) {
         super(fileName);
+        g = new HashGraph<>();
+        for(String s : input) {
+            var nodes = s.split("-");
+            g.addVertex(nodes[0]);
+            g.addVertex(nodes[1]);
+            g.addEdge(nodes[0], nodes[1], 1.0);
+        }
     }
 
     @Override
@@ -23,6 +36,7 @@ public class Day12 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        System.out.printf("The number of distinct path is %d\n", findPaths(g, "start", "end", false));
     }
 
     @Override
@@ -30,5 +44,83 @@ public class Day12 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        System.out.printf("The number of distinct path is %d\n", findPaths(g, "start", "end", true));
+    }
+
+    private int findPaths(Graph<String> g, String start, String end, boolean allowSmallCaveReEntrance) {
+        int pathCount = 0;
+
+        Queue<Path> q = new LinkedList<>();
+        Path path = new Path(start);
+        q.offer(path);
+
+        while(!q.isEmpty()) {
+            path = q.poll();
+            var last = path.get(path.size() - 1);
+
+            if(last.equals(end)) {
+                pathCount++;
+                continue;
+            }
+
+            for(var n : g.getNeighbors(last)) {
+                // Reject returning to start
+                if("start".equals(n)) {
+                    continue;
+                }
+                // If lower case and exists in the path, bad path
+                if(n.toLowerCase().equals(n)) {
+                    if(path.contains(n)) {
+                        if(allowSmallCaveReEntrance && path.canEnterSmallCaveAgain()) {
+                            Path newPath = new Path(path, n);
+                            newPath.enterSmallCave(n);
+                            q.offer(newPath);
+                            continue;
+                        }
+                        continue;
+                    }
+                }
+                Path newPath = new Path(path, n);
+                q.offer(newPath);
+            }
+        }
+        return pathCount;
+    }
+
+    private class Path {
+        List<String> nodes;
+        String smallCave;
+
+        public Path(String start) {
+            nodes = new ArrayList<>();
+            nodes.add(start);
+            smallCave = null;
+        }
+
+        public Path(Path p, String node) {
+            nodes = new ArrayList<>(p.nodes);
+            nodes.add(node);
+            smallCave = p.smallCave;
+        }
+
+        public int size() {
+            return nodes.size();
+        }
+
+        public String get(int i) {
+            return nodes.get(i);
+        }
+
+        public boolean contains(String node) {
+            return nodes.contains(node);
+        }
+
+        public boolean canEnterSmallCaveAgain() {
+            return smallCave == null;
+        }
+
+        public void enterSmallCave(String node) {
+            smallCave = node;
+        }
     }
 }
