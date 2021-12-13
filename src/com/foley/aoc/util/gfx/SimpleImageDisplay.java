7ac5c6d2e733559590b96bfda5eeaecd016019e5
heckler82 <heckler82@gmail.com>
2021-12-12 22:26:43 -0900

    Updated SimpleImageDisplay.java to just draw the image instead of using it as the icon for a JLabel and drawing that way

--- src/com/foley/aoc/util/gfx/SimpleImageDisplay.java
+++ src/com/foley/aoc/util/gfx/SimpleImageDisplay.java
@@ -1,8 +1,7 @@
 package com.foley.aoc.util.gfx;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Graphics;
 import java.awt.Image;
 
 /**
@@ -11,7 +10,35 @@ import java.awt.Image;
  * @author Evan Foley
  * @version 28 Nov 2020
  */
-public class SimpleImageDisplay{
+public class SimpleImageDisplay extends JFrame {
+    private Image img;
+
+    /**
+     * Creates a new image display for the specified image with the specified window title
+     *
+     * @param img the image to display
+     * @param title the title of the window
+     */
+    private SimpleImageDisplay(Image img, String title) {
+        super(title);
+        this.img = img;
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(img.getWidth(null), img.getHeight(null) + 39); // 39 is the height of the window menu bar
+        setResizable(true);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    /**
+     * Draws the image to the window
+     *
+     * @param g the graphics context
+     */
+    public void paint(Graphics g) {
+        var insets = getInsets();
+        g.drawImage(img, insets.left, insets.top, null);
+    }
+
     /**
      * Shows an image on the screen
      *
@@ -28,12 +55,13 @@ public class SimpleImageDisplay{
      * @param windowTitle the title of the window
      */
     public static void show(Image img, String windowTitle) {
-        JLabel label = new JLabel(new ImageIcon(img));
+        /**JLabel label = new JLabel(new ImageIcon(img));
         JFrame frame = new JFrame(windowTitle);
         frame.getContentPane().add(label);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        frame.setVisible(true);*/
+        var sid = new SimpleImageDisplay(img, windowTitle);
     }
 }
