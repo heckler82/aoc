1610b1c366d07e87f9d9890d65a60b9e74415e72
heckler82 <heckler82@gmail.com>
2021-12-15 21:48:47 -0900

    Added day 16 Solution

--- src/com/foley/aoc/year2021/Day16.java
+++ src/com/foley/aoc/year2021/Day16.java
@@ -2,6 +2,11 @@ package com.foley.aoc.year2021;
 
 import com.foley.aoc.util.Daily;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Solutions for day 16
  *
@@ -9,6 +14,10 @@ import com.foley.aoc.util.Daily;
  * @version 16 Dec 2021
  */
 public class Day16 extends Daily {
+    private Map<Character, String> map;
+    private String binaryString;
+    private List<Packet> packets;
+
     /**
      * Creates a new daily
      *
@@ -17,6 +26,14 @@ public class Day16 extends Daily {
      */
     public Day16(int year, String fileName) {
         super(year, fileName);
+        map = setupMap();
+        StringBuilder sb = new StringBuilder(); // Change this over to a map of char to string?
+        for(int i = 0; i < input[0].length(); i++) {
+            sb.append(map.get(input[0].charAt(i)));
+        }
+        binaryString = sb.toString();
+        packets = new ArrayList<>();
+        parsePacket(binaryString, 0, null);
     }
 
     @Override
@@ -24,6 +41,11 @@ public class Day16 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        long sum = 0L;
+        for(Packet p : packets) {
+            sum += p.ver;
+        }
+        System.out.printf("The sum of the version numbers is %d\n", sum);
     }
 
     @Override
@@ -31,5 +53,127 @@ public class Day16 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        System.out.printf("The value from the expression is %d\n", packets.get(0).getValue());
+    }
+
+    private int parsePacket(String str, int i, Packet parent) {
+        int ver = Integer.parseInt(str.substring(i, i + 3), 2);
+        i += 3;
+        int id = Integer.parseInt(str.substring(i, i + 3), 2);
+        i += 3;
+        Packet p = new Packet(ver, id);
+        packets.add(p);
+        if(parent != null) {
+            parent.packets.add(p);
+        }
+        if(id == 4) {
+            StringBuilder sb = new StringBuilder();
+            char c = '1';
+            while(c == '1') {
+                String s = str.substring(i, i + 5);
+                c = s.charAt(0);
+                sb.append(s.substring(1));
+                i += 5;
+            }
+            p.val = Long.parseLong(sb.toString(), 2);
+            return i;
+        } else {
+            int numPackets;
+            if(str.charAt(i++) == '0') { // Number of bits in subpackets
+                numPackets = Integer.parseInt(str.substring(i, i + 15), 2);
+                i += 15;
+                int res = 0;
+                while(numPackets > 0) {
+                    res = parsePacket(str.substring(i), 0, p);
+                    numPackets -= res;
+                    i += res;
+                }
+                return i;
+            } else { // Number of subpackets
+                numPackets = Integer.parseInt(str.substring(i, i + 11), 2);
+                i += 11;
+                while(numPackets > 0) {
+                    i += parsePacket(str.substring(i), 0, p);
+                    numPackets--;
+                }
+                return i;
+            }
+        }
+    }
+
+    private Map<Character, String> setupMap() {
+        var m = new HashMap<Character, String>();
+        m.put('0', "0000");
+        m.put('1', "0001");
+        m.put('2', "0010");
+        m.put('3', "0011");
+        m.put('4', "0100");
+        m.put('5', "0101");
+        m.put('6', "0110");
+        m.put('7', "0111");
+        m.put('8', "1000");
+        m.put('9', "1001");
+        m.put('A', "1010");
+        m.put('B', "1011");
+        m.put('C', "1100");
+        m.put('D', "1101");
+        m.put('E', "1110");
+        m.put('F', "1111");
+        return m;
+    }
+
+    private class Packet {
+        int ver;
+        int id;
+        long val;
+        List<Packet> packets;
+
+        public Packet(int ver, int id) {
+            this.ver = ver;
+            this.id = id;
+            this.val = -1L;
+            packets = new ArrayList<>();
+        }
+
+        public long getValue() {
+            switch(id) {
+                case 0:
+                    if(packets.size() == 1) {
+                        return packets.get(0).getValue();
+                    }
+                    return packets.stream().mapToLong(Packet::getValue).sum();
+                case 1:
+                    if(packets.size() == 1) {
+                        return packets.get(0).getValue();
+                    }
+                    return packets.stream().mapToLong(Packet::getValue).reduce(1L, (a, b) -> a * b);
+                case 2:
+                    return packets.stream().mapToLong(Packet::getValue).min().getAsLong();
+                case 3:
+                    return packets.stream().mapToLong(Packet::getValue).max().getAsLong();
+                case 4:
+                    return val;
+                case 5:
+                    if(packets.get(0).getValue() > packets.get(1).getValue()) {
+                        return 1L;
+                    } else {
+                        return 0L;
+                    }
+                case 6:
+                    if(packets.get(0).getValue() < packets.get(1).getValue()) {
+                        return 1L;
+                    } else {
+                        return 0L;
+                    }
+                case 7:
+                    if(packets.get(0).getValue() == packets.get(1).getValue()) {
+                        return 1L;
+                    } else {
+                        return 0L;
+                    }
+                default:
+                    return 0L;
+            }
+        }
     }
 }
