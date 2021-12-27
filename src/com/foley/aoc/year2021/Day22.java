3f6ec3dcb1ae8ae56421fe9f8bdbe5d4e41f9258
heckler82 <heckler82@gmail.com>
2021-12-26 17:40:41 -0900

    Started day 22 solutions

--- src/com/foley/aoc/year2021/Day22.java
+++ src/com/foley/aoc/year2021/Day22.java
@@ -1,6 +1,11 @@
 package com.foley.aoc.year2021;
 
 import com.foley.aoc.util.Daily;
+import com.foley.aoc.util.point.Point3D;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Solutions for day 22
@@ -9,6 +14,9 @@ import com.foley.aoc.util.Daily;
  * @version 22 Dec 2021
  */
 public class Day22 extends Daily {
+    private Set<Point3D> on;
+    private long onL;
+
     /**
      * Creates a new daily
      *
@@ -17,6 +25,8 @@ public class Day22 extends Daily {
      */
     public Day22(int year, String fileName) {
         super(year, fileName);
+        on = new HashSet<>();
+        onL = 0L;
     }
 
     @Override
@@ -24,6 +34,31 @@ public class Day22 extends Daily {
      * Accomplishes the first task for the day
      */
     public void task1() {
+        for(var s : input) {
+            boolean state = s.startsWith("on") ? true : false;
+            var axes = s.split(",");
+            var xAxis = axes[0].substring(axes[0].indexOf("=") + 1).split("\\.\\.");
+            var yAxis = axes[1].substring(axes[1].indexOf("=") + 1).split("\\.\\.");
+            var zAxis = axes[2].substring(axes[2].indexOf("=") + 1).split("\\.\\.");
+            int x1 = Integer.parseInt(xAxis[0]);
+            int x2 = Integer.parseInt(xAxis[1]);
+            int y1 = Integer.parseInt(yAxis[0]);
+            int y2 = Integer.parseInt(yAxis[1]);
+            int z1 = Integer.parseInt(zAxis[0]);
+            int z2 = Integer.parseInt(zAxis[1]);
+            for(int z = z1; z <= z2; z++) {
+                for(int y = y1; y <= y2; y++) {
+                    for(int x = x1; x <= x2; x++) {
+                        if(state) {
+                            on.add(new Point3D.Int(x, y, z));
+                        } else {
+                            on.remove(new Point3D.Int(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.printf("There are %d cubes turned on\n", on.size());
     }
 
     @Override
@@ -31,5 +66,6 @@ public class Day22 extends Daily {
      * Accomplishes the second task for the day
      */
     public void task2() {
+        System.out.printf("After the initialization process, there are %d cubes turned on\n", onL);
     }
 }
