b30712332d34ca786f160446352323fa37866140
Evan <heckler82@gmail.com>
2022-12-19 10:58:46 -0900

    Day 6 solution added

--- src/com/foley/aoc/year2022/Day6.java
+++ src/com/foley/aoc/year2022/Day6.java
@@ -2,6 +2,9 @@ package com.foley.aoc.year2022;
 
 import com.foley.aoc.util.Daily;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Solutions for day 6
  *
@@ -21,11 +24,42 @@ public class Day6 extends Daily {
 
     @Override
     protected void task1() {
-
+        System.out.printf("%d characters need to be processed before the first start-of-packet marker is detected\n", lookForMarker(4));
     }
 
     @Override
     protected void task2() {
+        System.out.printf("%d characters need to be processed before the first start-of-message marker is detected\n", lookForMarker(14));
+    }
 
+    /**
+     * Looks for a sequence of unique characters that is 'size' characters long
+     *
+     * @param size the size of the sequence to find
+     * @return the number of characters that must be processed to find the correct sized sequence
+     */
+    private int lookForMarker(int size) {
+        int numChars = 0;
+        Queue<Character> q = new LinkedList<>();
+        String stream = input[0];
+        for(int i = 0; i < stream.length(); i++) {
+            numChars++;
+            var c = stream.charAt(i);
+            if(!q.contains(c)) {
+                q.offer(c);
+                if(q.size() == size) {
+                    break;
+                }
+            } else {
+                while(!q.isEmpty()) {
+                    char polled = q.poll();
+                    if(polled == c) {
+                        break;
+                    }
+                }
+                q.offer(c);
+            }
+        }
+        return numChars;
     }
 }
